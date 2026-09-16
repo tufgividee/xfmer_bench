7f96fde787dcdf823aeefeb71652a800a9175373
tufgividee <tufgividee@gmail.com>
2026-09-16 16:41:30 -0400

    Track Multi30k download script

---
+++ data/multi30k/raw/download_data.sh
@@ -0,0 +1,10 @@
+#! /bin/bach
+
+wget https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/train.en.gz
+wget https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/train.de.gz
+wget https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/val.en.gz
+wget https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/val.de.gz
+wget https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/test_2016_flickr.en.gz
+wget https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/test_2016_flickr.de.gz
+
+gunzip *.gz
