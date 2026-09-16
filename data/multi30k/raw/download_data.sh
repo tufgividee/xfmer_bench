5568663ea092b82b144dd0529dfb2e91025c4a61
tufgividee <tufgividee@gmail.com>
2026-09-16 17:21:52 -0400

    updated wordlevel data prep script with prints

--- data/multi30k/raw/download_data.sh
+++ data/multi30k/raw/download_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 wget https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/train.en.gz
 wget https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/train.de.gz
 wget https://raw.githubusercontent.com/multi30k/dataset/master/data/task1/raw/val.en.gz
